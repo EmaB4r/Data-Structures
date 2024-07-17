8b4e4671972c25218033f9fea1f6115406ec89b3
EmaB4r <125159548+EmaB4r@users.noreply.github.com>
2024-07-17 16:41:22 +0200

    added list and stack

---
+++ List/list.c
@@ -0,0 +1,113 @@
+//
+// Created by EmanueleBar on 05/12/2023.
+//
+
+#include "list.h"
+#include <stdlib.h>
+
+list_t list_init(){
+    list_t l=(list_t)malloc(sizeof (struct list));
+    l->tail=l->head=NULL;
+    l->num_elem=0;
+    return l;
+}
+
+void list_recursive_free(node_t nodo, void(*free_func)(void*)){
+    if(nodo->next!=NULL){
+        list_recursive_free(nodo->next, free_func);
+        if(free_func!=NULL)
+            free_func(nodo->Item);
+        free(nodo);
+    }
+}
+
+void list_free(list_t list, void(*free_func)(void*)){
+    list_recursive_free(list->head, free_func);
+    free(list);
+}
+
+void list_ins_head(list_t list, void* Item){
+    if(list->head==NULL && list->tail==NULL){
+        list->head=list->tail= node_init();
+    }
+    else{
+        list->head->prev= node_init();
+        list->head->prev->next=list->head;
+        list->head=list->head->prev;
+    }
+    list->head->Item=Item;
+    list->num_elem+=1;
+
+}
+
+void list_ins_tail(list_t list, void* Item){
+    if(list->head==NULL && list->tail==NULL){
+        list->head=list->tail= node_init();
+    }
+    else{
+        list->tail->next= node_init();
+        list->tail->next->prev=list->tail;
+        list->tail=list->tail->next;
+    }
+    list->tail->Item=Item;
+    list->num_elem+=1;
+}
+
+
+int list_get_num_elem(list_t list){
+    return list->num_elem;
+}
+
+node_t node_init(){
+    node_t nodo=(node_t) malloc(sizeof (struct node));
+    nodo->next=NULL;
+    nodo->prev=NULL;
+    nodo->Item=NULL;
+    return nodo;
+}
+
+node_t list_get_head(list_t list){
+    return list->head;
+}
+
+node_t node_get_next(node_t node){
+    return node->next;
+}
+
+node_t node_get_prev(node_t node){
+    return node->prev;
+}
+
+void * node_get_val(node_t node){
+    return node->Item;
+}
+
+void list_print(list_t l, void (printing_fun)(void *)){
+    node_t n = list_get_head(l);
+    while (n!=NULL)
+    {
+        printing_fun(node_get_val(n));
+        n=node_get_next(n);
+    }
+}
+
+void list_insertion_sort(list_t l, int(*item_gt)(void*, void*)){
+    node_t n1=list_get_head(l);
+    node_t min, tmp;
+    void* tmp1;
+    while (n1!=NULL){
+        min=n1;
+        tmp=node_get_next(n1);
+        while(tmp!=NULL){
+            if(item_gt(node_get_val(min), node_get_val(tmp))){
+                min=tmp;
+            }
+            tmp= node_get_next(tmp);
+        }
+        tmp1=n1->Item;
+        n1->Item=min->Item;
+        min->Item=tmp1;
+
+        n1=node_get_next(n1);
+    }
+}
---
+++ List/list.h
@@ -0,0 +1,85 @@
+//
+// Created by EmanueleBar on 05/12/2023.
+//
+
+
+#ifndef LIST_H
+#define LIST_H
+typedef struct list *list_t;
+typedef struct node *node_t;
+
+//not reccomended to manually modify values
+typedef struct list {
+    node_t head;
+    node_t tail;
+    int num_elem;
+}*list_t;
+
+
+//not reccomended to manually modify values
+typedef struct node {
+    node_t prev;
+    node_t next;
+    void* Item;
+}*node_t;
+
+
+//creates a dynamic struct list, having headpointer, tailpointer and number of elements
+list_t list_init();
+
+
+//inserts a given item pointer in head's place
+void list_ins_head(list_t list, void* Item);
+
+//inserts a given item pointer in tail's place
+void list_ins_tail(list_t list, void* Item);
+
+//returns list's number of nodes
+int list_get_num_elem(list_t list);
+
+//creates a dynamic struc node
+node_t node_init();
+
+//returns list's head's pointer
+node_t list_get_head(list_t list);
+
+//returns
+node_t node_get_next(node_t node);
+
+//returns
+node_t node_get_prev(node_t node);
+
+//returns a node's value
+void * node_get_val(node_t node);
+
+//prints to screen the whole list's contents by using a user defined printing function
+//
+// if the item type saved in list is "int a"
+// print_fun should be defined as:
+// void a_print_fun(int* val){printf("%d", *val);}
+//
+// and then in main be called as:
+// list_print(list_t l, a_print_fun);
+//
+void list_print(list_t l, void (print_fun)(void *));
+
+//sorts list by using a user defined cmp function if(a > b)
+//
+// if the item type saved in list is "int a"
+// cmp should be defined as:
+// int a_gt_b(int* a, int*b){return *a>*b;}
+//
+// and then in main be called as:
+// list_insertion_sort(list_t l, a_gt_b);
+//
+void list_insertion_sort(list_t l, int(*item_gt)(void*, void*));
+
+//frees a list
+//
+// gets a list and a freeing function for custom allocations
+//
+// pass NULL if the items don't need to be freed
+//
+void list_free(list_t list, void(*free_func)(void*));
+
+#endif //LIST_H
